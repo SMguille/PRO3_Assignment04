1ff5592e486a569c2a82b7bbe76dd56e0563229b
Eduard Fekete <edko.fek@gmail.com>
2025-10-08 20:33:49 +0200

    [Add] Maven Springboot base

---
+++ retrieval/src/main/java/via/pro3/retrieval/RetrievalApplication.java
@@ -0,0 +1,14 @@
+package via.pro3.retrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class RetrievalApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(RetrievalApplication.class, args);
+  }
+
+}
---
+++ retrieval/src/test/java/via/pro3/retrieval/RetrievalApplicationTests.java
@@ -0,0 +1,13 @@
+package via.pro3.retrieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class RetrievalApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
