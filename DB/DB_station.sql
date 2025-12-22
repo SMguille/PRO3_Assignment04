cac19d9f75625f3af4884a1abc4f74156d492b93
Eduard Fekete <edko.fek@gmail.com>
2025-12-22 09:14:21 +0100

    [Update] settings, DDL

--- DB/DB_station.sql
+++ DB/DB_station.sql
@@ -4,8 +4,15 @@ SET SCHEMA 'pro3_assignment_station';
 
 -- Station 1
 
+DROP DATABASE IF EXISTS station1_db;
+CREATE DATABASE station1_db;
+
 -- Station 2
+
+DROP DATABASE IF EXISTS station2_db;
+CREATE DATABASE station2_db;
 
 -- Station 3
 
+DROP DATABASE IF EXISTS station3_db;
 CREATE DATABASE station3_db;
